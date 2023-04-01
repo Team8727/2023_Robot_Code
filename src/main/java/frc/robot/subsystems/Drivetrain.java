072a5f6e64d6dc8a9d63436740a6b03de3e7a1d2
dante <dante.can.hack@gmail.com>
2023-04-01 00:31:08 -0400

    auto is cracked now

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import static edu.wpi.first.math.system.plant.LinearSystemId.identifyDrivetrainSystem;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
 import com.pathplanner.lib.commands.PPRamseteCommand;
@@ -69,6 +70,8 @@ import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import com.pathplanner.lib.commands.FollowPathWithEvents;
+import java.util.HashMap;
 
 public class Drivetrain extends SubsystemBase {
   // Initalize motor controllers
@@ -210,6 +213,10 @@ public class Drivetrain extends SubsystemBase {
                     () -> driveVoltages(0, 0))
                 .until(() -> getRoll() > kAuto.chargeStopAngle)
                 .withTimeout(kAuto.creepTimeout));
+  }
+
+  public FollowPathWithEvents followPathwithEvents(PathPlannerTrajectory path, HashMap<String, Command> eventMap){
+    return new FollowPathWithEvents(this.followPath(path), path.getMarkers(), eventMap);
   }
 
   public Command followPath(PathPlannerTrajectory path) {
